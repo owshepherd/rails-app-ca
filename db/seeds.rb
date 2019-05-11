af48cdb4d61b321487e264c384339e6eb737d508
Robert De Matteo <robertdematteo@hotmail.com>
2019-05-11 20:17:49 +1000

    user bookings show view changed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# puts "Start of Seeding..."
-# # Cuisine.destroy_all
-# 10.times do
-#   Cuisine.new({ :cuisine_type => Faker::Restaurant.type }).save
-#   puts "Seeding Over"
-# end
+puts "Start of Seeding..."
+# Cuisine.destroy_all
+10.times do
+  Cuisine.new({ :cuisine_type => Faker::Restaurant.type }).save
+  puts "Seeding Over"
+end
 
 # # User.destroy.all
 # puts 'Creating an admin user'
@@ -23,11 +23,11 @@
 # puts regular.email
 
 
-Place.create!([
-{ "name": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
-{ "name": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
-{ "name": "Big Ben", "latitude": "51.500792", "longitude": "-0.124613"}
-])
+# Place.create!([
+# { "name": "Buckingham Palace", "latitude": "51.501564","longitude": "-0.141944"},
+# { "name": "Westminster Abbey", "latitude": "51.499581", "longitude": "-0.127309"},
+# { "name": "Big Ben", "latitude": "51.500792", "longitude": "-0.124613"}
+# ])
 
 
 
