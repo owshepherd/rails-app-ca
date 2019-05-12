05953703e3c1c5f859903bf945006497071c9a91
Owen <owshepherd@gmail.com>
2019-05-12 18:27:36 +1000

    Edit Listing Fixed

--- db/migrate/20190512070954_move_cuisine_id_to_provider.rb
+++ db/migrate/20190512070954_move_cuisine_id_to_provider.rb
@@ -1,9 +1,6 @@
 class MoveCuisineIdToProvider < ActiveRecord::Migration[5.2]
   def change
     add_column :providers, :cuisine_id, :integer
-    Listing.all.each do |l|
-      l.provider.update_attributes(cuisine_id: l.cuisine_id)
-    end
     remove_column :listings, :cuisine_id
   end
 end
